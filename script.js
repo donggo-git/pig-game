a1ac590801484940b2bab8e01c40af9b5f30846d
Dong <Dlttthvlhhth@gmail.com>
2021-01-21 21:43:48 -0800

    add roll effect

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+let roll = document.querySelector('.btn--roll');
+let dice = document.querySelector('.dice');
+let player0Point = 0;
+let player1Point = 0
+roll.addEventListener('click', function () {
+    let randomRoll = Math.round(Math.random() * 5 + 1);
+    dice.src = `dice-${randomRoll}.png`
+
+})
